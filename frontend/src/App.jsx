3d557b4319f6a521263731af58a20d5593083d40
AnishKumar100 <akcoder1102004@gmail.com>
2025-12-02 22:59:56 +0530

    Added Add features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import { AllRoutes } from './routes/AllRoutes'
 import 'quill/dist/quill.snow.css'
 
-export const App = () => 
-{
+export const App = () => {
   return (
-   <>
-   <AllRoutes/>
-   </>
+    <>
+      <AllRoutes />
+      <script
+        async="async"
+        data-cfasync="false"
+        src="//pl28177411.effectivegatecpm.com/08dc434734ea14f1c91b61d2e3c990d6/invoke.js"
+      ></script>
+      <div id="container-08dc434734ea14f1c91b61d2e3c990d6"></div>
+    </>
   )
 }
