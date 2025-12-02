98aa310f3170e6b112f3079a612b110ec0c6fcbf
AnishKumar100 <akcoder1102004@gmail.com>
2025-12-02 23:07:21 +0530

    Added Add features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,7 @@ export const App = () => {
   return (
     <>
       <AllRoutes />
-      <script
-        async="async"
-        data-cfasync="false"
-        src="//pl28177411.effectivegatecpm.com/08dc434734ea14f1c91b61d2e3c990d6/invoke.js"
-      ></script>
-      <div id="container-08dc434734ea14f1c91b61d2e3c990d6"></div>
+
     </>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
-import { NavBar,Header, BlogList, NewsLetter,Footer } from '../components/indexComponents'
+import { NavBar, Header, BlogList, NewsLetter, Footer } from '../components/indexComponents'
 
 export const Home = () => {
   return (
     <>
+      <script
+        async="async"
+        data-cfasync="false"
+        src="//pl28177411.effectivegatecpm.com/08dc434734ea14f1c91b61d2e3c990d6/invoke.js"
+      ></script>
+      <div id="container-08dc434734ea14f1c91b61d2e3c990d6"></div>
       <NavBar />
-      <Header/>
-      <BlogList/>
-      <NewsLetter/>
-      <Footer/>
+      <Header />
+      <BlogList />
+      <NewsLetter />
+      <Footer />
     </>
   )
 }
